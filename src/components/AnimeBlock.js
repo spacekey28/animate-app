235a7dc0be085a64e06af2afbb49dc6cb2b62ad7
Peter Son <peteson.me@gmail.com>
2021-03-12 22:51:23 +1300

    uninstall react-anime and applied react-spring

--- src/components/AnimeBlock.js
+++ src/components/AnimeBlock.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import Anime from 'react-anime';
-
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const AnimeBlock = () => {
-  let transition = {
-    opacity: [0, 1],
-    translateY: ['100vh', 0]
-  }
+  const [state, toggle] = useState(true)
+  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 1000 } });
 
   return (
-    <Anime {...transition}>
-      <div>Hello World!</div>
-    </Anime>
+    <div onClick={() => toggle(!state)}>
+      <animated.div
+        style={{
+          opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+          transform: x
+            .interpolate({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+            })
+            .interpolate(x => `scale(${x})`)
+        }}>
+        Hello React Spring
+      </animated.div>
+    </div>
   );
 }
 
